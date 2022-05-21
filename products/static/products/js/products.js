50437139dd6946a2fa0d3e578d18fc4c9548b902
dannymagnus <mrdanrichards@gmail.com>
2022-05-21 17:00:06 +0100

    add bag urls, template and context file

--- products/static/products/js/products.js
+++ products/static/products/js/products.js
@@ -2,12 +2,12 @@ function disableOption(){
     $(".size-option").each(function(){
         console.log($(this).val());
         // Test if the div element is empty
-        if($(this).attr("value") == 0){
+        if($(this).attr("data-count") == 0){
             $(this).attr("disabled", "disabled");
-            $(this).text($(this).attr("data-size")+" (Out of Stock)");
+            $(this).text($(this).attr("value")+" (Out of Stock)");
         }else{
             $(this).removeAttr('disabled');
-            $(this).text($(this).attr("data-size"));
+            $(this).text($(this).attr("value"));
         }
     });
 }
