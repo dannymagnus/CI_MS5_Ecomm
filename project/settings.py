d40624d1cd95920bff7015425eee19e31425258e
dannymagnus <mrdanrichards@gmail.com>
2022-05-30 09:44:02 +0100

    fix context processor products

--- project/settings.py
+++ project/settings.py
@@ -77,7 +77,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'bag.context_processors.bag_contents',
-                'products.context_processors.bag_contents',
+                'products.context_processors.brands',
             ],
         },
     },
